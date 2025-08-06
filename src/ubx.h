3f9c6875bbdff9c7e109c743143356fe7f12788f
Norwegia <65297640+Norwegia@users.noreply.github.com>
2025-08-06 20:07:26 +0200

    [feat] add functionality for seperate RTCM interface

--- src/ubx.h
+++ src/ubx.h
@@ -959,9 +959,10 @@ public:
 		Normal,                    ///< functionally redefined to mean exactly the same as RoverWithMovingBaseUART1
 		RoverWithMovingBase,       ///< expect RTCM input on UART2 from a moving base for heading output
 		MovingBase,                ///< RTCM output on UART2 to a rover (GPS is installed on the vehicle)
-		RoverWithMovingBaseUART1, ///< expect RTCM input on UART1 from a moving base for heading output
-		MovingBaseUART1,          ///< RTCM output on UART1 to a rover (GPS is installed on the vehicle)
+		RoverWithMovingBaseUART1,  ///< expect RTCM input on UART1 from a moving base for heading output
+		MovingBaseUART1,           ///< RTCM output on UART1 to a rover (GPS is installed on the vehicle)
 		RoverWithStaticBaseUart2,  ///< expect RTCM input on UART2 from a static base.
+		RTCMForwarder 			   ///< forwarding RTCM on a seperate, deditcated interface
 	};
 
 	GPSDriverUBX(Interface gpsInterface, GPSCallbackPtr callback, void *callback_user,
